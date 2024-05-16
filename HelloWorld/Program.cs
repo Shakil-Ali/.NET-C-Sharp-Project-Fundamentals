60d86c1996c7a5d0f9ef267874ba521a5678ba16
Shakil Ali <shakil_419@hotmail.co.uk>
2024-05-16 12:26:34 +0100

    Creating initial c# program

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+
+        }
+    }
+}
