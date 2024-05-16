81c8edb2f210957e086a4b8a508fcb0bdebca573
Shakil Ali <shakil_419@hotmail.co.uk>
2024-05-16 12:58:48 +0100

    Understanding data types and variables

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace HelloWorld
+﻿using System;
+
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.Write("Type your last name: ");
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
             Console.ReadLine();
 
         }
