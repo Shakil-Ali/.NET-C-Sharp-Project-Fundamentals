a7b1b2e470712e057df9a701427b939b64bb7db2
Shakil Ali <shakil_419@hotmail.co.uk>
2024-05-20 12:56:31 +0100

    Another way of doing a conditional if statement

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,28 +10,11 @@ namespace HelloWorld
             Console.WriteLine("Shakil's Big Giveaway");
             Console.Write("Choose a door: 1, 2 or 3: ");
             string userValue = Console.ReadLine();
-            string message = "";
 
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            
+            Console.WriteLine("You entered: {0}, therefore you won a {1}.", userValue, message);
 
-            if (userValue == "1")
-            {
-                message = "You won a new car!";
-            }
-            else if (userValue == "2") 
-            {
-                message = "You won a new boat!";
-            }
-            else if(userValue == "3") 
-            {
-                message = "You won a new plane!";
-            }
-            else 
-            {
-                message = "Sorry we didn't understand";
-                message += message + ". You Lose";
-            }
-
-            Console.WriteLine(message);
             Console.ReadLine();
 
         }
