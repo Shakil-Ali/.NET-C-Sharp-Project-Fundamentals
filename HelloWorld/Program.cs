d4e87026d54ecefe83bcc0228361d644dd3848ec
Shakil Ali <shakil_419@hotmail.co.uk>
2024-05-27 12:31:50 +0100

    Understanding arrays

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Common;
 
 namespace HelloWorld
 {
@@ -6,19 +7,39 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++) 
+            //int[] numbers = new int[5];
+
+            //numbers[0] = 4;
+            //numbers[1] = 8;
+            //numbers[2] = 15;
+            //numbers[3] = 16;
+            //numbers[4] = 23;
+
+            //Console.WriteLine(numbers[1]);
+            //Console.WriteLine(numbers.Length);
+            //Console.ReadLine();
+
+            //int[] numbers2 = new int[] { 1, 2, 3, 4 };
+
+            //string[] names = new string[] { "Shakil", "Ali" };
+
+            //for (int i = 0; i < names.Length; i++)
+            //{
+            //    Console.WriteLine(names[i]);
+            //}
+            //Console.ReadLine();
+
+            string zig = "You can get what you want out of life" +
+                " If you help enough other people get what they want.";
+
+            char[] charArray = zig.ToCharArray();
+            Array.Reverse(charArray);
+
+            foreach (char c in charArray )
             {
-                //Console.WriteLine(i);
-                if(i == 5)
-                {
-                    Console.WriteLine("Found 5!");
-                    break;
-                }
-            
+                Console.WriteLine(c);
             }
-
-            Console.ReadLine();
-
+            Console.WriteLine();
         }
     }
 }
