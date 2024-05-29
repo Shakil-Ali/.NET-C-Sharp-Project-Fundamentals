92b94e7ddcd9340f50663c8a427d0c14b47354d1
Shakil Ali <shakil_419@hotmail.co.uk>
2024-05-29 11:34:59 +0100

    Defining and call methods

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,39 +7,24 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //int[] numbers = new int[5];
-
-            //numbers[0] = 4;
-            //numbers[1] = 8;
-            //numbers[2] = 15;
-            //numbers[3] = 16;
-            //numbers[4] = 23;
-
-            //Console.WriteLine(numbers[1]);
-            //Console.WriteLine(numbers.Length);
-            //Console.ReadLine();
-
-            //int[] numbers2 = new int[] { 1, 2, 3, 4 };
-
-            //string[] names = new string[] { "Shakil", "Ali" };
-
-            //for (int i = 0; i < names.Length; i++)
-            //{
-            //    Console.WriteLine(names[i]);
-            //}
-            //Console.ReadLine();
-
-            string zig = "You can get what you want out of life" +
-                " If you help enough other people get what they want.";
-
-            char[] charArray = zig.ToCharArray();
-            Array.Reverse(charArray);
-
-            foreach (char c in charArray )
-            {
-                Console.WriteLine(c);
-            }
-            Console.WriteLine();
+            ReverseString("Shakil");
+            Console.ReadLine();
         }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        private static void ReverseString(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            foreach (char c in messageArray) 
+            {
+                Console.WriteLine(c);   
+            }
+        }
+
     }
 }
