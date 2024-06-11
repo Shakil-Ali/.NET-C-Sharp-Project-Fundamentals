eda672567ee5925b90f8d8e1ebe810d7e5b28b44
Shakil Ali <shakil_419@hotmail.co.uk>
2024-06-11 14:28:06 +0100

    Strings

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,54 +8,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            bool displayMenu = true;
-            while (displayMenu == true) 
-            {
-                displayMenu = MainMenu();   
-            }
+            string myString = "My \"so-called\" life";
+
+            Console.WriteLine(myString);
+            Console.ReadLine();
     
-        }
-
-        private static bool MainMenu()
-        {
-            Console.WriteLine("Choose an option: ");
-            Console.WriteLine("(1) Option 1");
-            Console.WriteLine("(2) Option 2");
-            Console.WriteLine("(3) Exit");
-
-            string result = Console.ReadLine();
-            if(result == "1")
-            {
-                PrintNumbers(); 
-                return true;
-            }
-            else if(result == "2") 
-            {
-                GuessingGame();
-                return true;    
-            }
-            else if(result == "3") 
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-
-        }
-
-
-        private static void PrintNumbers()
-        {
-            Console.WriteLine("Print numbers");
-            Console.ReadLine();
-        }
-
-        private static void GuessingGame()
-        {
-            Console.WriteLine("Guessing game!");
-            Console.ReadLine();
         }
 
 
