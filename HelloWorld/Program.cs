31da113a10b459a97c251013ec5b81fd7e4bed5d
Shakil Ali <shakil_419@hotmail.co.uk>
2024-06-11 17:32:54 +0100

    Collections

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Data;
 using System.Data.Common;
 using System.Security.AccessControl;
@@ -9,8 +10,16 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            
 
+            ArrayList myArrayList = new ArrayList();
+            myArrayList.Add("1");
+            myArrayList.Add("2");
+            myArrayList.Add("3");
+
+            foreach(int i in myArrayList)
+            {
+                Console.WriteLine(i);
+            }
 
         }
 
