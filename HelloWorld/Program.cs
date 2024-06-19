a4fe47898de12b20203f4cf182d3a567e06eece6
Shakil Ali <shakil_419@hotmail.co.uk>
2024-06-19 14:15:30 +0100

    Handling Exceptions

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,69 +11,31 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            List<Todo> todos = new List<Todo>()
-           {
-               new Todo { Description = "Task 1", EstimatedHours = 6, Status = Status.Completed},
-               new Todo { Description = "Task 2", EstimatedHours = 2, Status = Status.InProgress},
-               new Todo { Description = "Task 3", EstimatedHours = 8, Status = Status.NotStarted},
-               new Todo { Description = "Task 4", EstimatedHours = 12, Status = Status.Deleted},
-               new Todo { Description = "Task 1", EstimatedHours = 4, Status = Status.OnHold},
-           };
-
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-
-            PrintAssessment(todos);
+            try
+            {
+                string content = File.ReadAllText(@"C:\Lesson22\Example.txt");
+                Console.WriteLine(content);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the name of the file is right");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the directory is correct");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine(ex.Message);
+            }
             Console.ReadLine();
 
         }
 
-        private static void PrintAssessment(List<Todo> todos)
-        {
-            foreach (var todo in todos)
-            {
-                switch (todo.Status)
-                {
-                    case Status.NotStarted:
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        break;
-                    case Status.InProgress:
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        break;
-                    case Status.OnHold:
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        break;
-                    case Status.Completed:
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        break;
-                    case Status.Deleted:
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        break;
-                    default:
-                        break;
-                }
-                Console.WriteLine(todo.Description);
-
-            }
-        }
-
-
     }
-
-    class Todo
-    {
-        public string Description { get; set; }
-        public int EstimatedHours { get; set; }
-        public Status Status { get; set; }
-    }
-
-    enum Status
-    {
-        NotStarted,
-        InProgress,
-        OnHold,
-        Completed,
-        Deleted
-    }
-
+   
 
 }
